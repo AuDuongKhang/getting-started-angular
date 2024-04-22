750788b3a3ccff7afd1fb5903f79e69a1cf13de8
AuDuongKhang <duongkhang2100@gmail.com>
2024-04-22 14:00:06 +0700

    feature managing data

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,11 +11,16 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private cart: CartService) {}
 
   ngOnInit() {
     const routeParam = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParam.get('productId'));
     this.product = products.find((product) => product.id == productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cart.addToCart(product);
+    window.alert("This item has been added into cart");
   }
 }
